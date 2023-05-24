afed4e4126495ac1a2c577ba258bc0e5ffc6988a
MemduhCatalkaya <dtmemduh@gmail.com>
2023-05-24 15:47:19 +0300

    display info removed and some changes

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import tkinter as tk
 first_number = 0.0
 second_number = 0.0
 calculation = ""
-equal = True
+equal_clickable = True
+equal_clickable_2 = True
 no_number = True
 string_on = False
-equal2 = True
+calculation_on = False
 BG = "#BFBFBF"
+CAL_BG = "#DCDCDC"
+CAL_CLICKED_BG = "#B6FFA3"
 
 screen = tk.Tk()
 screen.configure(bg="white")
@@ -19,82 +22,63 @@ display = tk.Entry(calculator_frame, width=18, font=("times new roman", 40), sta
                    disabledbackground="white", disabledforeground="black", relief="sunken", border=8)
 display.pack(pady=5, padx=5)
 
-info_frame = tk.Frame(calculator_frame, bg=BG)
-info_frame.pack()
 
-display_fn = tk.Entry(info_frame, width=10, font=("times new roman", 30), state="disabled",
-                      disabledbackground="#EEEEEE", disabledforeground="black", relief="sunken", border=8)
-display_fn.grid(row=0, column=0, padx=3, pady=3)
-display_cal = tk.Entry(info_frame, width=2, font=("times new roman", 20), state="disabled",
-                       disabledbackground="#EEEEEE", disabledforeground="black", relief="sunken", border=8)
-display_cal.grid(row=0, column=1, padx=3, pady=3)
-display_sn = tk.Entry(info_frame, width=10, font=("times new roman", 30), state="disabled",
-                      disabledbackground="#EEEEEE", disabledforeground="black", relief="sunken", border=8)
-display_sn.grid(row=0, column=2, padx=3, pady=3)
+def button_bg_clear():
+    Button_multiply.config(bg=CAL_BG)
+    Button_plus.config(bg=CAL_BG)
+    Button_divide.config(bg=CAL_BG)
+    Button_percent.config(bg=CAL_BG)
+    Button_minus.config(bg=CAL_BG)
 
 
 def factorial_calculation(f):
     if f == 0:
-        return 1.0
+        return 1
     else:
         return f * factorial_calculation(f-1)
 
 
 def factorial():
-    global equal, string_on
+    global equal_clickable, string_on
     try:
         n = float(display.get())
         int_number = int(n)
+        display["state"] = "normal"
+        display.delete(0, tk.END)
         if n == int_number:
+            n = int(n)
             if 170 >= n >= 0:
                 num = factorial_calculation(n)
-                display["state"] = "normal"
-                display.delete(0, tk.END)
                 display.insert(tk.END, num)
-                display["state"] = "disabled"
             elif n > 170:
-                display["state"] = "normal"
-                display.delete(0, tk.END)
                 display.insert(tk.END, "INF!!")
-                display["state"] = "disabled"
-                equal = False
+                equal_clickable = False
                 string_on = True
             else:
-                display["state"] = "normal"
-                display.delete(0, tk.END)
                 display.insert(tk.END, "Negative Number!!")
-                display["state"] = "disabled"
-                equal = False
+                equal_clickable = False
                 string_on = True
         else:
-            display["state"] = "normal"
-            display.delete(0, tk.END)
             display.insert(tk.END, "Float Number!")
-            display["state"] = "disabled"
-            equal = False
+            equal_clickable = False
             string_on = True
+        display["state"] = "disabled"
     except ValueError:
-        pass
-    except OverflowError:
         pass
 
 
 def number_click(x):
-    global equal, no_number, string_on
-    if equal:
-        display["state"] = "normal"
-        display.insert(tk.END, x)
-        display["state"] = "disabled"
-    else:
-        display["state"] = "normal"
+    global equal_clickable, no_number, string_on, calculation_on
+    display["state"] = "normal"
+    if calculation_on:
         display.delete(0, tk.END)
-        display.insert(tk.END, x)
-        display["state"] = "disabled"
-        display_sn["state"] = "normal"
-        display_sn.delete(0, tk.END)
-        display_sn["state"] = "disabled"
-        equal = True
+        calculation_on = False
+    if not equal_clickable:
+        display.delete(0, tk.END)
+        equal_clickable = True
         string_on = False
+    display.insert(tk.END, x)
+    display["state"] = "disabled"
     no_number = False
 
 
@@ -102,167 +86,126 @@ def dot_click(x):
     if "." in display.get():
         pass
     else:
-        global equal, string_on
-        if equal:
-            display["state"] = "normal"
-            display.insert(tk.END, x)
-            display["state"] = "disabled"
-        else:
-            display["state"] = "normal"
+        global equal_clickable, string_on, calculation_on
+        display["state"] = "normal"
+        if calculation_on:
             display.delete(0, tk.END)
-            display.insert(tk.END, x)
-            display["state"] = "disabled"
-            equal = True
+            calculation_on = False
+        if not equal_clickable:
+            display.delete(0, tk.END)
+            equal_clickable = True
             string_on = False
+        display.insert(tk.END, x)
+        display["state"] = "disabled"
 
 
 def prc_click(prc):
-    if no_number or string_on:
+    global first_number, calculation, equal_clickable_2, calculation_on
+    if no_number or string_on or calculation == prc:
         pass
-    else:
-        global first_number, calculation, equal2
-        if calculation == prc:
-            pass
-        elif calculation == "":
-            first_number = display.get()
-            display_fn["state"] = "normal"
-            display_fn.delete(0, tk.END)
-            display_fn.insert(0, first_number)
-            display_fn["state"] = "disabled"
-            display_cal["state"] = "normal"
-            display_cal.delete(0, tk.END)
-            if prc == "*":
-                display_cal.insert(0, "X")
-            elif prc == "/":
-                display_cal.insert(0, "÷")
-            elif prc == "%":
-                display_cal.insert(0, "%")
-            else:
-                display_cal.insert(0, "+")
-            display_cal["state"] = "disabled"
-            display["state"] = "normal"
-            display.delete(0, tk.END)
-            display["state"] = "disabled"
-            calculation = prc
-            display_sn["state"] = "normal"
-            display_sn.delete(0, tk.END)
-            display_sn["state"] = "disabled"
-            equal2 = True
+    elif calculation == "":
+        first_number = display.get()
+        if prc == "*":
+            Button_multiply.config(bg=CAL_CLICKED_BG)
+        elif prc == "/":
+            Button_divide.config(bg=CAL_CLICKED_BG)
+        elif prc == "%":
+            Button_percent.config(bg=CAL_CLICKED_BG)
         else:
-            calculation = prc
-            display_cal["state"] = "normal"
-            display_cal.delete(0, tk.END)
-            if prc == "*":
-                display_cal.insert(0, "X")
-            elif prc == "/":
-                display_cal.insert(0, "÷")
-            elif prc == "%":
-                display_cal.insert(0, "%")
-            else:
-                display_cal.insert(0, "+")
-            display_cal["state"] = "disabled"
+            Button_plus.config(bg=CAL_CLICKED_BG)
+        calculation = prc
+        equal_clickable_2 = True
+        calculation_on = True
+    else:
+        calculation = prc
+        if prc == "*":
+            button_bg_clear()
+            Button_multiply.config(bg=CAL_CLICKED_BG)
+        elif prc == "/":
+            button_bg_clear()
+            Button_divide.config(bg=CAL_CLICKED_BG)
+        elif prc == "%":
+            button_bg_clear()
+            Button_percent.config(bg=CAL_CLICKED_BG)
+        else:
+            button_bg_clear()
+            Button_plus.config(bg=CAL_CLICKED_BG)
 
 
 def minus_click():
-    if len(display.get()) == 0:
-        display["state"] = "normal"
+    global first_number, calculation, equal_clickable_2, calculation_on
+    display["state"] = "normal"
+    if calculation_on:
+        display.delete(0, tk.END)
         display.insert(0, "-")
-        display["state"] = "disabled"
+        calculation_on = False
+    elif len(display.get()) == 0:
+        display.insert(0, "-")
     else:
-        if no_number or string_on:
+        if no_number or string_on or calculation == "-":
             pass
+        elif calculation == "":
+            first_number = display.get()
+            Button_minus.config(bg=CAL_CLICKED_BG)
+            calculation = "-"
+            equal_clickable_2 = True
+            calculation_on = True
         else:
-            global first_number, calculation, equal2
-            if calculation == "-":
-                pass
-            elif calculation == "":
-                first_number = display.get()
-                display_fn["state"] = "normal"
-                display_fn.delete(0, tk.END)
-                display_fn.insert(0, first_number)
-                display_fn["state"] = "disabled"
-                display_cal["state"] = "normal"
-                display_cal.delete(0, tk.END)
-                display_cal.insert(0, "-")
-                display_cal["state"] = "disabled"
-                display["state"] = "normal"
-                display.delete(0, tk.END)
-                display["state"] = "disabled"
-                calculation = "-"
-                display_sn["state"] = "normal"
-                display_sn.delete(0, tk.END)
-                display_sn["state"] = "disabled"
-                equal2 = True
-            else:
-                calculation = "-"
-                display_cal["state"] = "normal"
-                display_cal.delete(0, tk.END)
-                display_cal.insert(0, "-")
-                display_cal["state"] = "disabled"
+            calculation = "-"
+            button_bg_clear()
+            Button_minus.config(bg=CAL_CLICKED_BG)
+    display["state"] = "disabled"
 
 
 def equal_click():
-    global equal, equal2
-    if not equal:
-        pass
-    else:
-        if first_number == 0 or not equal2:
+    global equal_clickable, equal_clickable_2, second_number, calculation, calculation_on
+    if equal_clickable and equal_clickable_2 and not first_number == 0:
+        try:
+            global second_number, calculation
+            math = ""
+            second_number = display.get()
+            display["state"] = "normal"
+            display.delete(0, tk.END)
+            if calculation == "+":
+                math = float(first_number) + float(second_number)
+            elif calculation == "-":
+                math = float(first_number) - float(second_number)
+            elif calculation == "*":
+                math = float(first_number) * float(second_number)
+            elif calculation == "/":
+                math = float(first_number) / float(second_number)
+            elif calculation == "%":
+                math = float(first_number) * float(second_number) / 100
+            if math == int(math):
+                math = int(math)
+            display.insert(0, math)
+            display["state"] = "disabled"
+            calculation = ""
+            calculation_on = False
+            button_bg_clear()
+            equal_clickable = False
+            equal_clickable_2 = False
+            second_number = 0.0
+        except ValueError:
             pass
-        else:
-            try:
-                global second_number, calculation
-                equal = False
-                math = ""
-                second_number = display.get()
-                display_sn["state"] = "normal"
-                display_sn.delete(0, tk.END)
-                display_sn.insert(0, second_number)
-                display_sn["state"] = "disabled"
-                display["state"] = "normal"
-                display.delete(0, tk.END)
-                if calculation == "+":
-                    math = float(first_number) + float(second_number)
-                elif calculation == "-":
-                    math = float(first_number) - float(second_number)
-                elif calculation == "*":
-                    math = float(first_number) * float(second_number)
-                elif calculation == "/":
-                    math = float(first_number) / float(second_number)
-                elif calculation == "%":
-                    math = float(first_number) * float(second_number) / 100
-                display.insert(0, math)
-                display["state"] = "disabled"
-                calculation = ""
-                second_number = 0.0
-                display_cal["state"] = "normal"
-                display_cal.delete(0, tk.END)
-                display_cal["state"] = "disabled"
-                equal2 = False
-            except ValueError:
-                pass
+    else:
+        pass
 
 
 def clear_all():
     global first_number, second_number, display, calculation
-    global equal, no_number, string_on
+    global equal_clickable, no_number, string_on, calculation_on
     first_number = 0.0
     second_number = 0.0
     calculation = ""
-    display_cal["state"] = "normal"
-    display_cal.delete(0, tk.END)
-    display_cal["state"] = "disabled"
-    display_sn["state"] = "normal"
-    display_sn.delete(0, tk.END)
-    display_sn["state"] = "disabled"
-    display_fn["state"] = "normal"
-    display_fn.delete(0, tk.END)
-    display_fn["state"] = "disabled"
+    calculation_on = False
     display["state"] = "normal"
     display.delete(0, tk.END)
     display["state"] = "disabled"
-    equal = True
+    equal_clickable = True
     no_number = True
     string_on = False
+    button_bg_clear()
 
 
 def clear():
@@ -277,60 +220,23 @@ def clear():
 Button_Frame = tk.Frame(calculator_frame, bg=BG)
 Button_Frame.pack(padx=10, pady=10)
 
-Button_7 = tk.Button(Button_Frame, text=7, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=7: number_click(a), bg="#F6F6F6")
-Button_7.grid(row=1, column=0, padx=3, pady=3)
+button_list = [7, 8, 9, 4, 5, 6, 1, 2, 3]
 
-Button_8 = tk.Button(Button_Frame, text=8, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=8: number_click(a), bg="#F6F6F6")
-Button_8.grid(row=1, column=1, padx=3, pady=3)
-
-Button_9 = tk.Button(Button_Frame, text=9, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=9: number_click(a), bg="#F6F6F6")
-Button_9.grid(row=1, column=2, padx=3, pady=3)
-
-Button_4 = tk.Button(Button_Frame, text=4, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=4: number_click(a), bg="#F6F6F6")
-Button_4.grid(row=2, column=0, padx=3, pady=3)
-
-Button_5 = tk.Button(Button_Frame, text=5, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=5: number_click(a), bg="#F6F6F6")
-Button_5.grid(row=2, column=1, padx=3, pady=3)
-
-Button_6 = tk.Button(Button_Frame, text=6, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=6: number_click(a), bg="#F6F6F6")
-Button_6.grid(row=2, column=2, padx=3, pady=3)
-
-Button_1 = tk.Button(Button_Frame, text=1, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=1: number_click(a), bg="#F6F6F6")
-Button_1.grid(row=3, column=0, padx=3, pady=3)
-
-Button_2 = tk.Button(Button_Frame, text=2, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=2: number_click(a), bg="#F6F6F6")
-Button_2.grid(row=3, column=1, padx=3, pady=3)
-
-Button_3 = tk.Button(Button_Frame, text=3, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=3: number_click(a), bg="#F6F6F6")
-Button_3.grid(row=3, column=2, padx=3, pady=3)
-
-Button_0 = tk.Button(Button_Frame, text=0, width=6, height=2,
-                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                     command=lambda a=0: number_click(a), bg="#F6F6F6")
-Button_0.grid(row=4, column=1, padx=3, pady=3)
+for i, number in enumerate(button_list):
+    Button = tk.Button(Button_Frame, text=number, width=6, height=2,
+                       font=("times new roman", 18, "bold"), borderwidth=5, border=5,
+                       command=lambda a=number: number_click(a), bg="#F6F6F6")
+    Button.grid(row=i//3 + 1, column=i % 3, padx=3, pady=3)
 
 Button_dot = tk.Button(Button_Frame, text=".", width=6, height=2,
                        font=("times new roman", 18, "bold"), borderwidth=5, border=5,
                        command=lambda a=".": dot_click(a), bg="#F6F6F6")
 Button_dot.grid(row=4, column=0, padx=3, pady=3)
+
+Button_0 = tk.Button(Button_Frame, text=0, width=6, height=2,
+                     font=("times new roman", 18, "bold"), borderwidth=5, border=5,
+                     command=lambda a=0: number_click(a), bg="#F6F6F6")
+Button_0.grid(row=4, column=1, padx=3, pady=3)
 
 Button_equal = tk.Button(Button_Frame, text="=", width=6, height=2,
                          font=("times new roman", 18, "bold"), borderwidth=5, border=5,
@@ -339,22 +245,22 @@ Button_equal.grid(row=4, column=2, padx=3, pady=3)
 
 Button_plus = tk.Button(Button_Frame, text="+", width=6, height=2,
                         font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                        command=lambda a="+": prc_click(a), bg="#DCDCDC")
+                        command=lambda a="+": prc_click(a), bg=CAL_BG)
 Button_plus.grid(row=4, column=3, padx=3, pady=3)
 
 Button_minus = tk.Button(Button_Frame, text="-", width=6, height=2,
                          font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                         command=minus_click, bg="#DCDCDC")
+                         command=minus_click, bg=CAL_BG)
 Button_minus.grid(row=3, column=3, padx=3, pady=3)
 
 Button_multiply = tk.Button(Button_Frame, text="x", width=6, height=2,
                             font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                            command=lambda a="*": prc_click(a), bg="#DCDCDC")
+                            command=lambda a="*": prc_click(a), bg=CAL_BG)
 Button_multiply.grid(row=2, column=3, padx=3, pady=3)
 
 Button_divide = tk.Button(Button_Frame, text="÷", width=6, height=2,
                           font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                          command=lambda a="/": prc_click(a), bg="#DCDCDC")
+                          command=lambda a="/": prc_click(a), bg=CAL_BG)
 Button_divide.grid(row=1, column=3, padx=3, pady=3)
 
 Button_clear_all = tk.Button(Button_Frame, text="CA", width=6, height=2,
@@ -374,7 +280,7 @@ Button_factorial.grid(row=0, column=1, padx=3, pady=3)
 
 Button_percent = tk.Button(Button_Frame, text="%", width=6, height=2,
                            font=("times new roman", 18, "bold"), borderwidth=5, border=5,
-                           bg="#DCDCDC", command=lambda a="%": prc_click(a))
+                           bg=CAL_BG, command=lambda a="%": prc_click(a))
 Button_percent.grid(row=0, column=2, padx=3, pady=3)
 
 screen.mainloop()
